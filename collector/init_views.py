7072d57d94bb61952b94aa4cb07cc4154d301c62
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-09-01 13:29:06 +0100

    add all necessary views

--- collector/init_views.py
+++ collector/init_views.py
@@ -1,7 +1,7 @@
 import couchdb
 from couchdb.design import ViewDefinition
 
-MAP1 = """
+SOURCES_KEY_OWNER_MAP = """
 function(doc) {
   if(doc['owner_id'] && doc['csp-report'] && doc['csp-report']['blocked-uri'] && doc['csp-report']['violated-directive']) {
   emit( [ doc['owner_id'],
@@ -11,11 +11,11 @@ function(doc) {
  }
 }
 """
-REDUCE1 = """
+SOURCES_KEY_OWNER_RED = """
 function(k,v,re) { return true; }
 """
 
-MAP2 = """
+ALL_BY_OWNER = """
 function(doc) {
   if(doc['owner_id'] && doc['csp-report'] && doc['meta']) {
   emit( doc['owner_id'], null );
@@ -23,6 +23,33 @@ function(doc) {
 }
 """
 
+APPROVED_SOURCES_OWNER_MAP="""
+function(doc) {
+  if(doc.approved_uri && doc.approved_type) {
+	emit([doc.owner_id, doc.approved_type, doc.approved_uri], null);
+  }
+}
+"""
+APPROVED_SOURCES_OWNER_RED="""
+function(k,v,r) {
+return true;
+}
+"""
+
+BY_SOURCE_TYPE_MAP="""
+function(doc) {
+  if(doc['owner_id'] && doc['csp-report'] && doc['csp-report']['blocked-uri'] && doc['csp-report']['violated-directive']) {
+  emit( [ doc['owner_id'],
+          doc['csp-report']['blocked-uri'],
+          doc['csp-report']['violated-directive'].split(' ')[0] ],
+	  null );
+ }
+}
+"""
+
+
 db = couchdb.Server('http://127.0.0.1:5984/')['csp']
-ViewDefinition('csp', 'sources_key_owner', map_fun=MAP1, reduce_fun=REDUCE1).sync(db)
-ViewDefinition('csp', 'all_by_owner', map_fun=MAP2).sync(db)
+ViewDefinition('csp', 'sources_key_owner', map_fun=SOURCES_KEY_OWNER_MAP, reduce_fun=SOURCES_KEY_OWNER_RED).sync(db)
+ViewDefinition('csp', 'all_by_owner', map_fun=ALL_BY_OWNER).sync(db)
+ViewDefinition('csp', 'approved_sources_owner', map_fun=APPROVED_SOURCES_OWNER_MAP, reduce_fun=APPROVED_SOURCES_OWNER_RED).sync(db)
+ViewDefinition('csp', 'by_source_type', map_fun=BY_SOURCE_TYPE_MAP).sync(db)
