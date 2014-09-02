b67e7d0832b3f1dca560b709dd258ac1c9167f53
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-09-02 21:31:53 +0100

    temp

--- static/controllers/CspAnalysisController.js
+++ static/controllers/CspAnalysisController.js
@@ -1,29 +1,5 @@
 cspControllers.controller('CspAnalysisController', ['$scope', '$cookieStore', 'cornercouch', '$window',
     function($scope, $cookieStore, cornercouch, $window) {
 
-        $scope.owner_id = $cookieStore.get('owner_id');
-        if(!$scope.owner_id) { $window.location.href='/static/#/login'; }
-
-        $scope.blocked = true;
-        $scope.db = cornercouch(couchdb_url, 'GET').getDB('csp');
-        $scope.index = 0;
-        $scope.db.query("csp", "sources_key_owner",
-            {
-                include_docs: false,
-                // CouchDB idiom used to narrow search
-                // ref: http://docs.couchdb.org/en/latest/couchapp/views/collation.html#string-ranges
-                startkey: [Math.floor($scope.owner_id)],
-                endkey: [Math.floor($scope.owner_id),{}],
-                // group required for reduce function to work
-                group: true
-            }).success( function() {
-                    console.log('data loading finished');
-                    $scope.blocked = false;
-                }
-            );
-
-
-
-
 
 ]);
