e643e6bbced7d17cd4ebae31b7bd528c337c7c90
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-09-03 11:07:47 +0100

    reformat code

--- static/controllers/CspAnalysisController.js
+++ static/controllers/CspAnalysisController.js
@@ -1,8 +1,10 @@
 cspControllers.controller('CspAnalysisController', ['$scope', '$cookieStore', 'cornercouch', '$window',
-    function($scope, $cookieStore, cornercouch, $window) {
+    function ($scope, $cookieStore, cornercouch, $window) {
 
         $scope.owner_id = $cookieStore.get('owner_id');
-        if(!$scope.owner_id) { $window.location.href='/static/#/login'; }
+        if (!$scope.owner_id) {
+            $window.location.href = '/static/#/login';
+        }
 
         $scope.blocked = true;
         $scope.db = cornercouch(couchdb_url, 'GET').getDB('csp');
@@ -12,29 +14,29 @@ cspControllers.controller('CspAnalysisController', ['$scope', '$cookieStore', 'c
             // CouchDB idiom used to narrow search
             // ref: http://docs.couchdb.org/en/latest/couchapp/views/collation.html#string-ranges
             startkey: [Math.floor($scope.owner_id)],
-            endkey: [Math.floor($scope.owner_id),{}],
+            endkey: [Math.floor($scope.owner_id), {}],
             // group required for reduce function to work
             group: true
         })
-        .success( function() {
-            console.log('data loading finished');
-            $scope.blocked = false;
-        });
+            .success(function () {
+                console.log('data loading finished');
+                $scope.blocked = false;
+            });
 
-        $scope.logout = function() {
+        $scope.logout = function () {
             console.log('logout');
             $cookieStore.remove('owner_id');
-            $window.location.href='/static/#/login';
+            $window.location.href = '/static/#/login';
         };
 
-        $scope.detail_close = function() {
+        $scope.detail_close = function () {
             console.log('detail_close ');
             delete $scope.meta;
             delete $scope.raw_report;
         };
 
-        $scope.detail_open = function(index) {
-            console.log('detail_open '+index);
+        $scope.detail_open = function (index) {
+            console.log('detail_open ' + index);
             $scope.reviewed = false;
             $('#report-row-' + $scope.index).removeClass('bg-info'); // delete highlight from old row
             $scope.index = index;
@@ -42,75 +44,83 @@ cspControllers.controller('CspAnalysisController', ['$scope', '$cookieStore', 'c
             // sources list already contains the key we can use to fetch sample report
             $scope.db2 = cornercouch(couchdb_url, 'GET').getDB('csp');
             $scope.db2.query('csp', 'by_source_type',
-            {
-                limit: 1,
-                startkey: $scope.db.rows[index].key, // endkey not needed because limit=1
-                include_docs: true
-            }).success( function() {
-                $scope.csp = $scope.db2.rows[0].doc['csp-report'];
-                $scope.meta = $scope.db2.rows[0].doc.meta;
-                $scope.norm_type = $scope.csp['violated-directive'].split(' ')[0];
-                $scope.norm_src = normalize_csp_source($scope.csp);
-            }
+                {
+                    limit: 1,
+                    startkey: $scope.db.rows[index].key, // endkey not needed because limit=1
+                    include_docs: true
+                }).success(function () {
+                    $scope.csp = $scope.db2.rows[0].doc['csp-report'];
+                    $scope.meta = $scope.db2.rows[0].doc.meta;
+                    $scope.norm_type = $scope.csp['violated-directive'].split(' ')[0];
+                    $scope.norm_src = normalize_csp_source($scope.csp);
+                }
             );
         };   // detail_open
 
-        $scope.reset_approved = function() {
+        $scope.reset_approved = function () {
             $scope.db2 = cornercouch(couchdb_url, 'GET').getDB('csp');
             $scope.db2.query('csp', 'approved_sources_owner', {
                 reduce: false,
                 startkey: [Math.floor($scope.owner_id)],
-                endkey: [Math.floor($scope.owner_id,{})],
+                endkey: [Math.floor($scope.owner_id, {})],
                 include_docs: true
             })
-            .success( function() {
-                delete_list = { 'docs': [] };
-                $scope.db2.rows.forEach( function(item) {
-                    delete_list.docs.push({
-                        '_id': item.doc._id,
-                        '_rev': item.doc._rev,
-                        '_deleted' : true
+                .success(function () {
+                    delete_list = { 'docs': [] };
+                    $scope.db2.rows.forEach(function (item) {
+                        delete_list.docs.push({
+                            '_id': item.doc._id,
+                            '_rev': item.doc._rev,
+                            '_deleted': true
+                        });
                     });
+                    // run bulk delete - CornerCouch does not support it
+                    client = new XMLHttpRequest();
+                    client.open('POST', couchdb_url + '/csp/_bulk_docs');
+                    client.setRequestHeader('Content-Type', 'application/json');
+                    client.send(JSON.stringify(delete_list));
                 });
-                // run bulk delete - CornerCouch does not support it
+        };  // reset_approved
+
+        $scope.review_source = function (allow) {
+
+            console.log('review_source allow=' + allow);
+
+            // highlight processed row according to its state
+            if (allow) {
+                $('#report-row-' + $scope.index).addClass('bg-success');
+            } else {
+                $('#report-row-' + $scope.index).addClass('bg-warning');
+            }
+
+            if (allow) {
+                // save new whitelist entry if action was to allow
+                db2 = cornercouch(couchdb_url, 'GET').getDB('csp');
+                newdoc = {  'owner_id': Math.floor($scope.owner_id),
+                    'approved_uri': $scope.norm_src,
+                    'approved_type': $scope.norm_type
+                };
+                db2.newDoc(newdoc).save();
+            }
+            // mark as approved on the page
+            $scope.reviewed = true;
+
+            // set all reports with this key as reviewed
+            $scope.db2.query('csp', 'by_source_type', {
+                key: [Math.floor($scope.owner_id), $scope.norm_type, $scope.norm_src],
+                include_docs: true
+            }).success(function () {
+                approve_list = { 'docs': [] };
+                $scope.db2.rows.forEach(function (item) {
+                    item.doc['reviewed'] = true;
+                    approve_list.docs.push(item.doc);
+                });
+                // run bulk update
                 client = new XMLHttpRequest();
                 client.open('POST', couchdb_url + '/csp/_bulk_docs');
                 client.setRequestHeader('Content-Type', 'application/json');
-                client.send(JSON.stringify(delete_list));
+                client.send(JSON.stringify(approve_list));
             });
-        };  // reset_approved
-
-        $scope.review_source = function(allow) {
-
-                console.log('review_source allow=' + allow);
-                if(allow) {
-                    // save new whitelist entry if action was to allow
-                    db2 = cornercouch(couchdb_url, 'GET').getDB('csp');
-                    newdoc = {  'owner_id'     : Math.floor($scope.owner_id),
-                                'approved_uri' : $scope.norm_src,
-                                'approved_type': $scope.norm_type
-                                };
-                    db2.newDoc(newdoc).save();
-                }
-                // mark as approved on the page
-                $scope.reviewed = true;
-
-                 // set all reports with this key as reviewed
-                 $scope.db2.query('csp', 'by_source_type', {
-                        key: [Math.floor($scope.owner_id), $scope.norm_type, $scope.norm_src],
-                        include_docs: true
-                    }).success( function() {
-                        approve_list = { 'docs': [] };
-                        $scope.db2.rows.forEach( function(item) {
-                            item.doc['reviewed'] = true;
-                            approve_list.docs.push(item.doc);
-                        });
-                        // run bulk update
-                        client = new XMLHttpRequest();
-                        client.open('POST', couchdb_url + '/csp/_bulk_docs');
-                        client.setRequestHeader('Content-Type', 'application/json');
-                        client.send(JSON.stringify(approve_list));
-                    });
 
         }; // approve_source
 
--- static/controllers/CspReportsController.js
+++ static/controllers/CspReportsController.js
@@ -1,8 +1,10 @@
 cspControllers.controller('CspReportsController', ['$scope', '$cookieStore', 'cornercouch', '$window',
-    function($scope, $cookieStore, cornercouch, $window) {
+    function ($scope, $cookieStore, cornercouch, $window) {
 
         $scope.owner_id = $cookieStore.get('owner_id');
-        if(!$scope.owner_id) { $window.location.href='/static/#/login'; }
+        if (!$scope.owner_id) {
+            $window.location.href = '/static/#/login';
+        }
 
         $scope.blocked = true; // for infinite scroll
         $('#reports-prev-button').addClass('disabled');
@@ -14,21 +16,21 @@ cspControllers.controller('CspReportsController', ['$scope', '$cookieStore', 'co
                 include_docs: true,
                 key: Math.floor($scope.owner_id),
                 limit: screenRows('#reports-left-list')
-            }).success( function() {
-                    console.log('data loading finished');
-                    $scope.blocked = false;
-                    $scope.detail_show(0);
-                }
-            );
+            }).success(function () {
+                console.log('data loading finished');
+                $scope.blocked = false;
+                $scope.detail_show(0);
+            }
+        );
 
-        $scope.logout = function() {
+        $scope.logout = function () {
             console.log('logout');
             $cookieStore.remove('owner_id');
-            $window.location.href='/static/#/login';
+            $window.location.href = '/static/#/login';
         };
 
-        $scope.detail_show = function(index) {
-            console.log('detail_show '+index);
+        $scope.detail_show = function (index) {
+            console.log('detail_show ' + index);
             $('#reports-li-' + $scope.index).removeClass('bg-info'); // remove highlight from previous
             $scope.index = index; // update scope index
             $('#reports-li-' + $scope.index).addClass('bg-info'); // highlight
@@ -37,12 +39,12 @@ cspControllers.controller('CspReportsController', ['$scope', '$cookieStore', 'co
             $scope.meta = $scope.db.rows[index].doc['meta'];
             $scope.raw = 0;
 
-            if($scope.index==0) {
+            if ($scope.index == 0) {
                 $('#reports-prev-button').addClass('disabled');
             } else {
                 $('#reports-prev-button').removeClass('disabled');
             }
-            if($scope.index==($scope.db.rows.length-1)) {
+            if ($scope.index == ($scope.db.rows.length - 1)) {
                 $('#reports-next-button').addClass('disabled');
             } else {
                 $('#reports-next-button').removeClass('disabled');
@@ -50,68 +52,68 @@ cspControllers.controller('CspReportsController', ['$scope', '$cookieStore', 'co
 
         };
 
-        $scope.detail_prev = function() {
+        $scope.detail_prev = function () {
             console.log('detail_prev ' + $scope.index);
-            if($scope.index>0) {
-                $scope.detail_show($scope.index-1);
+            if ($scope.index > 0) {
+                $scope.detail_show($scope.index - 1);
             }
         };
 
-        $scope.detail_next = function() {
+        $scope.detail_next = function () {
             console.log('detail_next ' + $scope.index);
-            if($scope.index<$scope.db.rows.length) {
-                $scope.detail_show($scope.index+1);
+            if ($scope.index < $scope.db.rows.length) {
+                $scope.detail_show($scope.index + 1);
             }
         };
 
-        $scope.load_next_page = function() {
+        $scope.load_next_page = function () {
             console.log('load next page');
-            $scope.db.queryMore().success( function() {
-                    console.log('data loading finished');
-                    $scope.blocked = false;
-                });
+            $scope.db.queryMore().success(function () {
+                console.log('data loading finished');
+                $scope.blocked = false;
+            });
         };
 
-         $scope.open_raw = function(index) {
+        $scope.open_raw = function (index) {
             console.log('open_raw');
             $scope.show_raw = true;
             $scope.show_original = false;
             $scope.show_violated = false;
-         };
-         $scope.open_original = function(index) {
+        };
+        $scope.open_original = function (index) {
             $scope.show_raw = false;
             $scope.show_original = true;
             $scope.show_violated = false;
-         };
-         $scope.open_violated = function(index) {
+        };
+        $scope.open_violated = function (index) {
             $scope.show_raw = false;
             $scope.show_original = false;
             $scope.show_violated = true;
-         };
+        };
 
-         $scope.delete_all = function() {
+        $scope.delete_all = function () {
             $scope.db2 = cornercouch(couchdb_url, 'GET').getDB('csp');
             $scope.db2.query('csp', 'all_by_owner', {
                 key: Math.floor($scope.owner_id),
                 include_docs: true
             })
-            .success( function() {
-                delete_list = { 'docs': [] };
-                $scope.db2.rows.forEach( function(item) {
-                    delete_list.docs.push({
-                        '_id': item.doc._id,
-                        '_rev': item.doc._rev,
-                        '_deleted' : true
+                .success(function () {
+                    delete_list = { 'docs': [] };
+                    $scope.db2.rows.forEach(function (item) {
+                        delete_list.docs.push({
+                            '_id': item.doc._id,
+                            '_rev': item.doc._rev,
+                            '_deleted': true
+                        });
                     });
+                    // run bulk delete - CornerCouch does not support it
+                    client = new XMLHttpRequest();
+                    client.open('POST', couchdb_url + '/csp/_bulk_docs');
+                    client.setRequestHeader('Content-Type', 'application/json');
+                    client.send(JSON.stringify(delete_list));
+                    location.reload();
                 });
-                // run bulk delete - CornerCouch does not support it
-                client = new XMLHttpRequest();
-                client.open('POST', couchdb_url + '/csp/_bulk_docs');
-                client.setRequestHeader('Content-Type', 'application/json');
-                client.send(JSON.stringify(delete_list));
-                location.reload();
-            });
-         };
+        };
 
 
     }
