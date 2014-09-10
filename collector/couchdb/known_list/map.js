e8feed25dcd4b45de7bc29c20b75a3ae08b2091d
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-09-10 10:17:43 +0100

    reformat

--- collector/couchdb/known_list/map.js
+++ collector/couchdb/known_list/map.js
@@ -1,9 +1,9 @@
 /**
  * Created by pawelkrawczyk on 09/09/2014.
  */
-function(doc) {
-  if(doc['owner_id'] && doc['review_type'] && doc['review_source'] && doc['review_action']) {
-    emit([doc['owner_id'], doc['review_type'], doc['review_source'], doc['review_action']]
-	, null)
-}
+function (doc) {
+    if (doc['owner_id'] && doc['review_type'] && doc['review_source'] && doc['review_action']) {
+        emit([doc['owner_id'], doc['review_type'], doc['review_source'], doc['review_action']]
+            , null)
+    }
 }
