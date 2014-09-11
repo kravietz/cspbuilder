600af39b324dd835b79a2893769c95dc06f0cc0c
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-09-11 16:57:39 +0100

    further fixes in known list management logic

--- api/api.py
+++ api/api.py
@@ -23,87 +23,85 @@ app = Flask(__name__)
 server = Server('http://localhost:5984/')
 db = server['csp']
 
-
 # TODO: set & verify CSRF headers
 # TODO: authentication
 @app.route('/api/<owner_id>/review', methods=['POST'])
-def review_type_source(owner_id):
+def update_known_list(owner_id):
     start_time = datetime.now(timezone.utc)
     client_ip = request.environ.get('REMOTE_ADDR')
 
     data = request.json
 
     try:
-        review_type = data['review_type']
+        review_directive = data['review_type']
         review_source = data['review_source']
         review_action = data['review_action']
     except KeyError:
-        print('review_type_source {} {} invalid input: {}'.format(start_time, client_ip, data))
+        print('update_known_list {} {} invalid input: {}'.format(start_time, client_ip, data))
         abort(400)
 
-    # save known list entry for auto-reviewing of future reports
-    results = db.view('csp/known_list',
-                       include_docs=True, reduce=False,
-                       startkey=[owner_id, review_type, review_source],
-                       endkey=[owner_id, review_type, {}])
+    # update known list entry
+    # first check if there's such entry already
+    results = db.view('csp/known_list', include_docs=True, startkey=[owner_id], endkey=[owner_id, {}])
 
-    known_list_doc = {}
+    match = False
 
-    print('review_type_source results {} for {} {}'.format(len(results), review_type, review_source))
+    for row in results:
+        # "key":["9018643792216450862","font-src","https://fonts.gstatic.com","accept"]
+        # filter by directive
+        if row.key[1] == review_directive:
+            # filter by source
+            # so we might be adding "font-src","https://fonts.gstatic.com"
+            # while "font-src","https://fonts.gstatic.com/some/other/" is already there
+            if fnmatch(row.key[2], review_source + '*'):
+                # new entry is shorter and more general - use it
+                row.doc['review_source'] = review_source
+                db.save[row.doc]
+                print('update_known_list saved updated KL entry {}'.format(row.doc))
+                match = True
 
-    if not len(results):
-        # no entries for this type and source were found - add a new one
+    if not match:
+        # means the KL was not updated, create a new entry
         known_list_doc = {
             'owner_id': owner_id,
-            'review_type': review_type,
+            'review_type': review_directive,
             'review_source': review_source,
             'review_action': review_action,
             # for audit
             'client_ip': client_ip,
             'timestamp': start_time.isoformat(),
         }
-        print('review_type_source saving new {}'.format(known_list_doc))
         db.save(known_list_doc)
-    else:
-        # entries were found, just leave one and update its action
-        first = True
-        for row in results:
-            print('review_type_source updating existing {}'.format(row.doc))
-            if first:
-                known_list_doc = row.doc
-                known_list_doc['review_action'] = review_action
-                known_list_doc['client_ip'] = client_ip
-                known_list_doc['timestamp'] = start_time.isoformat()
-                db.save(known_list_doc)
-                first = False
-            else:
-                db.delete(known_list_doc)
+        print('update_known_list saved new KL entry {}'.format(known_list_doc))
 
-    # convert review command to document status
-    if review_action == 'accept':
-            action = 'accepted'
-    if review_action == 'reject':
-            action = 'rejected'
+    action_to_status = {'accept': 'accepted', 'reject': 'rejected'}
+    report_status = action_to_status[review_action]
 
     # review old reports matching the pattern (using bulk interface)
     docs = []
-    for row in db.view('csp/grouped_types_sources',
-                       include_docs=True, reduce=False,
-                       startkey=[owner_id, review_type, review_source],
-                       endkey=[owner_id, review_type, {}]):
-        doc = row.doc
-        doc['reviewed'] = action
-        # save the known list entry used to review this report
-        doc['review_rule'] = [owner_id, review_type, review_source, action]
-        doc['review_method'] = 'user'
-        docs.append(doc)
+    i = 0
+    for row in db.view('csp/1000_owner_type_src', include_docs=True,
+                       startkey=[owner_id, review_directive], endkey=[owner_id, review_directive, {}]):
+        # ["9018643792216450862", "connect-src", "http://api.mixpanel.com"]
+        if fnmatch(row.key[2], review_source + '*'):
+            doc = row.doc
+            doc['reviewed'] = report_status
+            # save the known list entry used to review this report
+            doc['review_rule'] = [owner_id, review_directive, review_source, review_action]
+            doc['review_method'] = 'user'
+            docs.append(doc)
+            i += 1
 
-    db.update(docs)
+    if docs:
+        db.update(docs)
+
+    print('update_known_list updated status of {} existing reports'.format(i))
 
     stop_time = datetime.now(timezone.utc)
-    print('review_type_source {} {} {} {}'.format(start_time, client_ip, request.url, stop_time - start_time))
+    print('update_known_list {} {} {} {}'.format(start_time, client_ip, request.url, stop_time - start_time))
 
     return '', 204, []
+
 
 @app.route('/api/<owner_id>/all-reports', methods=['DELETE'])
 def delete_all_reports(owner_id):
@@ -116,7 +114,7 @@ def delete_all_reports(owner_id):
         doc['_deleted'] = True
         docs.append(doc)
 
-    if(docs):
+    if docs:
         db.update(docs)
 
     stop_time = datetime.now(timezone.utc)
@@ -124,9 +122,9 @@ def delete_all_reports(owner_id):
 
     return '', 204, []
 
+
 @app.route('/report/<owner_id>', methods=['POST'])
 def read_csp_report(owner_id):
-
     start_time = datetime.now(timezone.utc)
 
     mimetype = request.headers['Content-Type']
@@ -177,6 +175,7 @@ def read_csp_report(owner_id):
     action = 'unknown'
     review_rule = 'default'
 
+    # TODO: violated_directive could be used in CouchDB filter as it's static string
     results = db.view('csp/known_list', startkey=[owner_id], endkey=[owner_id, {}])
 
     print('read_csp_report found {} KL entries for report: {} {}'.format(len(results), violated_directive, blocked_uri))
@@ -206,7 +205,7 @@ def read_csp_report(owner_id):
 
     output['review_rule'] = review_rule
     output['review_method'] = 'auto'
-    action_to_status = { 'accept': 'accepted', 'reject': 'rejected'}
+    action_to_status = {'accept': 'accepted', 'reject': 'rejected'}
     output['reviewed'] = action_to_status[action]
 
     # save current report to CouchDB
@@ -214,11 +213,14 @@ def read_csp_report(owner_id):
 
     stop_time = datetime.now(timezone.utc)
 
-    print('read_csp_report {} {} {} {} action={} owner={} violated-directive={} blocked-uri={}'.format(start_time, client_ip,
-                                                                                       request.url,
-                                                                                       stop_time - start_time, action,
-                                                                                       owner_id,
-                                                                                       violated_directive, blocked_uri))
+    print('read_csp_report {} {} {} {} action={} owner={} violated-directive={} blocked-uri={}'.format(start_time,
+                                                                                                       client_ip,
+                                                                                                       request.url,
+                                                                                                       stop_time - start_time,
+                                                                                                       action,
+                                                                                                       owner_id,
+                                                                                                       violated_directive,
+                                                                                                       blocked_uri))
 
     return '', 204, []
 
