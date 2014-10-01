e26b10ff27c72bfda7ed7f9a363df9495d3c8e7d
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-01 16:30:41 +0100

    do not redirect to /login if user not logged in

--- static/controllers/CspSessionController.js
+++ static/controllers/CspSessionController.js
@@ -8,13 +8,10 @@ cspControllers.controller('CspSessionController', ['$scope', '$cookieStore', '$w
         // check if user is logged in
         var owner_id = $cookieStore.get('owner_id');
         console.log('CspSessionController owner_id=' + owner_id + ' (encoded)');
-        if (!owner_id) {
-            // redirect to login page
-            $window.location.href = '/static/#/login';
-        } else {
+        if (owner_id) {
             // decode owner_id from BASE64 and store in root scope
             $rootScope.owner_id = window.atob(owner_id);
-            console.log('CspSessionController owner_id=' + $rootScope.owner_id + ' (decoded)')
+            console.log('CspSessionController owner_id=' + $rootScope.owner_id + ' (decoded)');
         }
 
         $scope.logout = function () {
@@ -23,7 +20,7 @@ cspControllers.controller('CspSessionController', ['$scope', '$cookieStore', '$w
             delete $rootScope.owner_id;
             $cookieStore.remove('owner_id');
             $cookieStore.remove('XSRF-TOKEN');
-            //$window.location.href = '/';
+            $window.location.href = '/';
             console.log('logout coookies after=' + document.cookie);
         }
 
