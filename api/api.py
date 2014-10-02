4e8bd8cf1dd48d5b30c2c486e3108085e8810e97
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-02 14:32:14 +0100

    add debugging message to CF-Connecting-IP

--- api/api.py
+++ api/api.py
@@ -35,7 +35,7 @@ db = server.database('csp')
 def get_client_ip():
     client_ip = IPAddress(request.environ.get('REMOTE_ADDR'))
     for net in CLOUDFLARE_IPS:
-
+        print('get_client_ip', client_ip, net, client_ip in net)
         if client_ip in net:
             # this is CloudFlare network, try to extract real IP
             cf_ip = request.headers.get('CF-Connecting-IP')
