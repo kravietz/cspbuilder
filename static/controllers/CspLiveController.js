7725836ab76a9e8ab26a09cf6604d6476398014d
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-08 22:42:19 +0100

    move polling into separate function

--- static/controllers/CspLiveController.js
+++ static/controllers/CspLiveController.js
@@ -14,20 +14,32 @@ cspControllers.controller('CspLiveController', ['$scope', '$rootScope', '$interv
         mixpanel.track("View live");
 
         $scope.reports = [];
+        var last_seq = null;
 
         var promise = $interval(function () {
-            $http.get('/csp/_changes?feed=longpoll&filter=csp/owner&limit=10&owner_id=' + $rootScope.owner_id)
+            poll($rootScope.owner_id, last_seq);
+        }, 2000);
+
+        function poll(owner_id, last_seq) {
+            var req = '/csp/_changes?descending=true&feed=longpoll&filter=csp/owner&limit=10';
+            req += '&owner_id=' + owner_id;
+            if (last_seq) {
+                req += '&last_seq=' + last_seq;
+            }
+            $http.get(req)
                 .success(function (data, status, headers, config) {
                     console.log('get', data);
                     $scope.reports = data.results;
-                    $scope.last_seq = data.last_seq;
+                    last_seq = data.last_seq;
                 })
                 .error(function (data, status, headers, config) {
                     $scope.error = data;
                 });
-        }, 2000);
+
+        };
 
         $scope.stop = function () {
+            console.log('Cancelled');
             $interval.cancel(promise);
         }
 
