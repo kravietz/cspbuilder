3322a9acc619fe18cba0fcb75cdf9b0bd7a10ee7
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-08 11:35:17 +0100

    track views

--- static/controllers/CspSessionController.js
+++ static/controllers/CspSessionController.js
@@ -5,6 +5,9 @@
 cspControllers.controller('CspSessionController', ['$scope', '$cookieStore', '$window', '$rootScope',
     function ($scope, $cookieStore, $window, $rootScope) { "use strict";
 
+        // track page view, this should be called on each page including main
+        mixpanel.track("Page view");
+
         // check if user is logged in
         var owner_id = $cookieStore.get('owner_id');
         console.log('CspSessionController owner_id=' + owner_id + ' (encoded)');
