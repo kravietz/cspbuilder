398e23f2e8b182e748b97cae69df8034e5e38de1
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-15 12:33:41 +0100

    implement cleanup API

--- api/api.py
+++ api/api.py
@@ -55,6 +55,55 @@ def login_response(owner_id):
     print('login_response setting token cookie {}'.format(token))
     return resp
 
+
+@app.route('/cleanup/week', methods=['POST'])
+def cleanup_api():
+    """
+    Delete alerts that are older than 7 days. This function is gateway to the actual
+    cleanup function that is started in background thread.
+    """
+    t = threading.Thread(target=cleanup, daemon=True)
+    t.start()
+    print('cleanup_api started thread {} {} alive={}'.format(t.name, t.ident, t.is_alive()))
+    return '', 204, []
+
+
+def cleanup():
+    """
+    Delete alerts that are older than 7 days. This the actual cleanup thread implementation.
+    """
+    print('cleanup thread starting')
+    lv = threading.local()
+    lv.start_time = datetime.now(timezone.utc)
+
+    lv.results = True
+    lv.total = 0
+    # updating is done in batches; views can return thousands
+    # of documents, which ends up in timeouts and excessive memory usage
+    while lv.results:
+        lv.i = 0
+        lv.docs = []
+
+        for row in db.query('csp/1910_week_old', include_docs=True, limit=1000, skip=lv.total):
+            # just copy the elements that are required to delete the document
+            lv.docs.append({'_id': row['doc']['_id'], '_rev': row['doc']['_rev']})
+            # total is used as offset for skip
+            lv.total += 1
+            # i is used to see if we got any rows at all
+            lv.i += 1
+
+        # does the database still return results?
+        # it's done this way because py-couchdb returns generator
+        lv.results = lv.i > 0
+        print('i=', lv.i, 'total=', lv.total, 'lv.docs=', len(lv.docs))
+        if len(lv.docs):
+            db.delete_bulk(lv.docs)
+
+    lv.run_time = datetime.now(timezone.utc) - lv.start_time
+
+    print('cleanup updated status of {} existing reports, time {}'.format(lv.total, lv.run_time))
+
+
 @app.route('/policy/<owner_id>/', methods=['GET'])
 def policy(owner_id):
     start_time = datetime.now(timezone.utc)
@@ -188,7 +237,7 @@ def update_known_list(owner_id):
 
 
 def review_old_reports(owner_id, review_directive, review_source, review_action):
-    print('starting review_old_reports thread')
+    print('review_old_reports thread starting')
     lv = threading.local()
     lv.start_time = datetime.now(timezone.utc)
 
@@ -243,7 +292,7 @@ def review_old_reports(owner_id, review_directive, review_source, review_action)
 
     lv.run_time = datetime.now(timezone.utc) - lv.start_time
 
-    print('update_known_list updated status of {} existing reports, time {}'.format(lv.total, lv.run_time))
+    print('review_old_reports updated status of {} existing reports, time {}'.format(lv.total, lv.run_time))
 
 
 @app.route('/api/<owner_id>/all-reports', methods=['DELETE'])
