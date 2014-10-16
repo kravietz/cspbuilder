c43307ca1f3529c97d2993b80ad7fd02bd9e9d07
Pawel Krawczyk <pawel.krawczyk@hush.com>
2014-10-16 21:06:22 +0100

    new features

--- util.py
+++ util.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from couchdb import Server
+import pycouchdb
 
-__author__ = 'pawelkrawczyk'
+server = pycouchdb.Server('http://localhost:5984/')
+db = server.database('csp')
 
-server = Server('http://localhost:5984/')
-db = server['csp']
-db.compact()
+for row in db.query('csp/1910_week_old', include_docs=True, limit=10):
+    print(row)
 
-i=0
-for row in db.view('csp/_all_docs', include_docs=True):
-    if 'owner_id' in row.doc:
-        db.delete(row.doc)
+exit()
+
+results=True
+total=0
+owner='6578275293912231771'
+directive='script-src'
+while results:
+    i = 0
+    docs = []
+    for row in db.query('csp/1300_unknown', include_docs=True, limit=1000, startkey=[owner, directive, 'self'], endkey=[owner,directive,{}], skip=total):
+        docs.append({ '_id': row['doc']['_id'], '_rev': row['doc']['_rev']})
         i += 1
+        total += 1
+    results = i > 0
+    print(i, total)
+    if len(docs):
+        db.delete_bulk(docs)
 
-print(i,'deleted')
+print(total,'deleted')
+db.compact()
