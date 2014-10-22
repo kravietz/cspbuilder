03dea4bbb1cc73a792a99095873b8b86a9d5e335
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-10-22 16:34:55 +0100

    generate new policy in different formats

--- static/controllers/CspMainController.js
+++ static/controllers/CspMainController.js
@@ -5,14 +5,13 @@
 cspControllers.controller('CspMainController', ['$scope',
     function ($scope) { "use strict";
 
-        $scope.new_csp = function () {
+        $scope.new_csp = function (format) {
             var a = Math.random().toString().replace('.','');
             var b = Math.random().toString().replace('.','');
             var new_owner_id = a + b;
             new_owner_id = new_owner_id.substring(1, 19); // skip leading 0
             var approved_list = empty_approved_list();
             var csp_config = default_csp_config();
-            var format = 'generic';
             var policy = policy_generator(new_owner_id, format, csp_config, approved_list);
             $scope.new_policy = policy[0];
             $scope.new_owner_id = new_owner_id;
