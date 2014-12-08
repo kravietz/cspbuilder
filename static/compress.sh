1136ded8d5740eca547f4ceb13e67fdcc2f3eb27
Pawel Krawczyk <pawel.krawczyk@hush.com>
2014-12-08 11:33:48 +0000

    minify & compress

--- static/compress.sh
+++ static/compress.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-find . -type f -name '*.js' -exec gzip -9fk {} \;
-find . -type f -name '*.html' -exec gzip -9fk {} \;
-find . -type f -name '*.css' -exec gzip -9fk {} \;
-find . -type f -name '*.map' -exec gzip -9fk {} \;
+for f in $(ls app/*.js | egrep -v 'min.js$' | xargs); do
+        n=$(dirname $f)/$(basename $f .js).min.js
+        python -m jsmin $f > $n
+done
+for f in $(ls controllers/*.js | egrep -v 'min.js$' | xargs); do
+        n=$(dirname $f)/$(basename $f .js).min.js
+        python -m jsmin $f > $n
+done
+find . | egrep '\.(html|map|svg|ttf|css|js)$' | xargs gzip -9kf 
