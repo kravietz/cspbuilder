f885681c995f2f82597636f0c9f430472bf954fd
Pawel Krawczyk <pawel.krawczyk@hush.com>
2014-12-08 14:04:05 +0000

    support Apple icons per https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html

--- static/compress.sh
+++ static/compress.sh
@@ -8,3 +8,10 @@ for f in $(ls controllers/*.js | egrep -v 'min.js$' | xargs); do
         python -m jsmin $f > $n
 done
 find . | egrep '\.(html|map|svg|ttf|css|js)$' | xargs gzip -9kf 
+cp logo.png apple-touch-icon.png
+cp apple-touch-icon.png touch-icon-ipad.png
+cp apple-touch-icon.png touch-icon-iphone-retina.png
+cp apple-touch-icon.png touch-icon-ipad-retina.png
+mogrify -geometry 76x76 touch-icon-ipad.png
+mogrify -geometry 120x120 touch-icon-iphone-retina.png
+mogrify -geometry 152x152 touch-icon-ipad-retina.png
