8ec7401fbf4aa2b58cb180cce024be3ea31e2fdb
Pawel Krawczyk <p.krawczyk@kainos.com>
2014-12-09 16:18:11 +0000

    Django generator stub

---
+++ static/app/django.js
@@ -0,0 +1,11 @@
+/**
+ * Created by pawelkrawczyk on 09/12/14.
+ */
+
+function django_generator() {
+    // TODO: https://github.com/kravietz/django-security
+    var policy = "";
+    var policy_message = 'This format is intended to be directly usable with <a href="https://github.com/kravietz/django-security">django-security</a>.';
+
+    return [policy, policy_message];
+}
