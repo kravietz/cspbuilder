6564644d192b3f054828b5bb91438ec8b7569ecd
Pawel Krawczyk <pawel.krawczyk@hush.com>
2014-12-14 23:54:22 +0000

    fixes and improvements after testing

--- api/sbf.py
+++ api/sbf.py
@@ -7,24 +7,31 @@ import pycouchdb
 
 
 class SBF(object):
-    SBF_DOC_ID = '_internal/bloom_filter'
-    SBF_FILE_NAME = 'sbf.dat'
+    doc_id = 'bloom_filter'
+    file_name = 'sbf.dat'
     db = None
-    sbf = None
+    f = None
+    error_rate = 0.01
 
     def _save_sbf(self):
         buf = BytesIO()
-        self.sbf.tofile(buf)
+        self.f.tofile(buf)
         buf.seek(0)
-        _doc = {'_id': self.SBF_DOC_ID}
-        doc = self.db.save(_doc)
-        self.db.put_attachment(doc, buf, filename=self.SBF_FILE_NAME)
+        try:
+            doc = self.db.get(self.doc_id)
+        except pycouchdb.exceptions.NotFound:
+            _doc = {'_id': self.doc_id}
+            doc = self.db.save(_doc)
+        self.db.put_attachment(doc, buf, filename=self.file_name, content_type='application/octet-stream')
+
+    def save(self):
+        self._save_sbf()
 
     def _load_sbf(self):
         ret = None
         try:
-            doc = self.db.get(self.SBF_DOC_ID)
-            ret = self.db.get_attachment(doc, self.SBF_FILE_NAME)
+            doc = self.db.get(self.doc_id)
+            ret = self.db.get_attachment(doc, self.file_name)
         except pycouchdb.exceptions.NotFound:
             pass
 
@@ -34,11 +41,13 @@ class SBF(object):
 
         return ret
 
-    def __init__(self, db):
+    def __init__(self, db, doc_id='bloom_filter', file_name='sbf.dat'):
+        self.doc_id = doc_id
+        self.file_name = file_name
         self.db = db
-        self.sbf = ScalableBloomFilter()
-        self.sbf = self._load_sbf()
-        if not self.sbf:
+        self.f = ScalableBloomFilter()
+        self.f = self._load_sbf()
+        if not self.f:
             # need to create new SBF
-            self.sbf = ScalableBloomFilter(mode=ScalableBloomFilter.LARGE_SET_GROWTH, error_rate=0.01)
+            self.f = ScalableBloomFilter(mode=ScalableBloomFilter.LARGE_SET_GROWTH, error_rate=self.error_rate)
             self._save_sbf()
