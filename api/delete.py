18d8e0a665a295dee87ff751423788f556912332
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-04-21 12:47:07 +0100

    reimplement deletion procedure to query database in batches

--- api/delete.py
+++ api/delete.py
@@ -4,6 +4,9 @@
 import datetime
 import threading
 
+import pycouchdb
+
+
 __author__ = 'Paweł Krawczyk'
 
 
@@ -14,37 +17,45 @@ def delete_all_reports_task(owner_id, db, verbose=False):
 
     print('delete_all_reports_thread starting for {} at {}'.format(owner_id, lv.start_time))
 
-    lv.docs = []
-    lv.i = 0
     lv.total = 0
+    lv.more_results = True
+    lv.processed = 0
 
-    # cycle through all reports for this user
-    for row in db.query('csp/1200_all', key=owner_id, include_docs=True):
+    while lv.more_results:
+        lv.deleted = 0
+        lv.docs = []
+        lv.i = 0
 
-        # skip non CSP report entries - such as KL entries
-        if 'csp-report' not in row['doc']:
-            continue
+        for row in db.query('csp/1200_all', key=owner_id, include_docs=True, limit=1000, skip=lv.processed):
 
-        # copy the document's crucial parts (id, rev), adding the _deleted flag
-        lv.doc = {'_id': row['doc']['_id'], '_rev': row['doc']['_rev'], '_deleted': True}
-        lv.docs.append(lv.doc)
-        lv.i += 1
-        lv.total += 1
+            lv.processed += 1
 
-        if verbose:
-            print('.', end='')
+            # skip non CSP report entries - such as KL entries
+            if 'csp-report' not in row['doc']:
+                continue
 
-        # process in batches to prevent long run-time and memory hogging
-        if lv.i > 200:
+            # this item can be deleted, just copy the elements that are required to delete it
+            lv.docs.append({'_id': row['doc']['_id'], '_rev': row['doc']['_rev']})
+            lv.deleted += 1
+
+            # i is used to see if we got any rows at all
+            lv.i += 1
+
             if verbose:
-                print('\ndelete_all_reports_thread deleting {}, total {}', lv.i, lv.total)
-            db.save_bulk(lv.docs)
-            lv.i = 0
-            lv.docs = []
+                print('.', end='')
 
-    # complete any remaining records
-    if lv.docs:
-        db.save_bulk(lv.docs)
+        # does the database still return results?
+        # it's done this way because py-couchdb returns generator
+        lv.more_results = lv.i > 0
+
+        # purge this batch
+        if len(lv.docs):
+            if verbose:
+                print('\ndelete_all_reports_thread deleting batch of {} reports'.format(len(lv.docs)))
+            try:
+                db.delete_bulk(lv.docs)
+            except pycouchdb.exceptions.Conflict:
+                pass
 
     lv.stop_time = datetime.datetime.now(datetime.timezone.utc)
 
