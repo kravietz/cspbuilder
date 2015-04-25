53750cadaa0ad08885304e71157efc76f43a26f3
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-04-25 21:02:24 +0100

    not sys but socket

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import socket
 
 __author__ = 'Paweł Krawczyk'
 
 ALLOWED_CONTENT_TYPES = ['application/json', 'application/csp-report']
 CSRF_KEY = 'fxwL8Ole62zSUXOk8LYKQlMweLs'
-DEVELOPER_MACHINE = sys.gethostname().endswith('.lan')
+DEVELOPER_MACHINE = socket.gethostname().endswith('.lan')
