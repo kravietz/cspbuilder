2b1d3c861a186da6148daf854d1274668587cdbe
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-04-25 19:58:20 +0100

    port remaining views

---
+++ designs/reports/views/1110_source_urls/reduce.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Paweł Krawczyk on 25/04/15.
+ */
+_sum
---
+++ designs/reports/views/1120_source_type_url/map.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Paweł Krawczyk on 25/04/15.
+ */
+(function (doc) {
+    if (doc['owner_id'] && doc['csp-report'] && doc['csp-report']['blocked-uri']) {
+        if (!doc['review'] || (doc['review'] && doc['review']['decision'] === 'unknown')) {
+            blocked_uri = doc['csp-report']['blocked-uri'];
+            if (/^data:/.test(blocked_uri)) {
+                // truncate the BASE64 encoded data from URI
+                blocked_uri = blocked_uri.split(',')[0];
+            }
+            if (/^https?/.test(blocked_uri)) {
+                // truncate URI to exclude params after ? or #
+                blocked_uri = blocked_uri.match(/^(https?:\/\/[^?#]+)/)[1];
+                blocked_uri = blocked_uri.split('/').slice(0, 3).join('/');
+            }
+
+            emit([doc['csp-report']['violated-directive'].split(' ')[0], blocked_uri,], 1);
+
+        }
+    }
+});
