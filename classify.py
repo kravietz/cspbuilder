7920c2325710ba9464b4d5accb461e5985e2c08e
Pawel Krawczyk <p.krawczyk@kainos.com>
2015-04-25 21:27:55 +0100

    use configurable CLASSIFY_INTERVAL for the loop timeout

--- classify.py
+++ classify.py
@@ -14,6 +14,7 @@ import pycouchdb
 
 from apihelpers.known import KnownList
 from apihelpers.utils import REPORTS_DB_PREFIX
+from settings import CLASSIFY_INTERVAL
 
 
 __author__ = 'Paweł Krawczyk'
@@ -152,7 +153,7 @@ if __name__ == '__main__':
             # by changes_feed() to the callback
             try:
                 server.database(db).changes_feed(ReportsFeedReader(), filter='reports/unclassified', since=last_seq,
-                    timeout=50)
+                    timeout=CLASSIFY_INTERVAL)
             # this means the poll timed out and server returned empty line, just skip over it
             except ValueError:
                 pass
--- settings.py
+++ settings.py
@@ -7,3 +7,4 @@ __author__ = 'Paweł Krawczyk'
 ALLOWED_CONTENT_TYPES = ['application/json', 'application/csp-report']
 CSRF_KEY = 'fxwL8Ole62zSUXOk8LYKQlMweLs'
 DEVELOPER_MACHINE = socket.gethostname().endswith('.lan')
+CLASSIFY_INTERVAL = 200
